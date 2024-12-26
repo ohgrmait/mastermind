cd5218cc82ddbdc484402bb1a8fd82251a0f4291
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-26 21:16:57 +0530

    Allow both Human and Computer to play Mastermind

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,50 +1,45 @@
+# frozen_string_literal: true
+
 require_relative 'game'
 require_relative 'human'
 require_relative 'computer'
 
-class Mastermind
+class Mastermind # rubocop:disable Style/Documentation,Metrics/ClassLength
   TURNS = 12
 
   def initialize
     @game = Game.new
+    @game_status = nil
   end
 
   def start_game
     show_prompt
-    puts ''
     user_input
-    puts ''
-    # store_code
     play_game
-    # end_game
+    end_game
   end
 
   def show_prompt
-    puts '=> A game of Mastermind between two players: Human and Computer!'
+    puts '...A game of Mastermind between two players: Human and Computer!'
     puts ''
-    puts "=> You can guess the opponent's secret code within twelve turns;"
+    puts "...You can guess the opponent's secret code within twelve turns;"
     puts '   at each turn you get a feedback (or clues) that tells you how'
     puts '   good the guess was, whether it is exactly correct (black peg)'
     puts '   or it was the exact color but in the wrong space (white peg).'
     puts ''
-    puts '=> [Availability - Blue, Orange, Yellow, Green, Purple, and Red]'
+    puts '...Color options - Blue, Orange, Yellow, Green, Purple, and Red!'
+    puts "...Example code - 'GGGG', 'BGOY', 'GRPY', 'RRRR', 'PPOB', 'RRRR'"
+    puts ''
   end
 
   def user_input
-    print '=> Will you (c)reate or (g)uess the code? '
+    print '...Do you wanna (c)reate or (g)uess the code? '
     choice = gets.chomp
     @game.assign(choice)
   end
 
-  # def store_code
-  #   @code = @game.create_code
-  #   puts "=> The #{@game.code_maker} has created the secret code."
-  #   puts "   [use]: 'GGGG' means all the green colors."
-  #   puts ''
-  # end
-
-  def print_guess(guess)
-    print "\t"
+  def print_guess(guess) # rubocop:disable Metrics/CyclomaticComplexity,Metrics/MethodLength
+    print '     '
     guess.chars.each do |char|
       case char
       when 'B'
@@ -64,7 +59,7 @@ class Mastermind
   end
 
   def print_feedback(feedback)
-    print "\t"
+    print ' '
     black = feedback[0]
     white = feedback[1]
     black.times do
@@ -76,53 +71,50 @@ class Mastermind
     (4 - (black + white)).times { print '  ' }
   end
 
-  def print_stats(guess, feedback)
+  def print_result(guess, feedback)
     puts ''
     print_guess(guess)
     print_feedback(feedback)
-    puts "\t   #{feedback[0]} black | #{feedback[1]} white"
-    puts ''
+    puts "\t\t\t#{feedback[0]} black, #{feedback[1]} white"
   end
 
-  # def end_game
-  #   if @game.feedback[0] != 4
-  #     puts "=> #{@game.code_breaker}, you have exhausted all the possible turns!"
-  #     puts '=> Game over! You were unable to find out the code.'
-  #   elsif @game.feedback[0] == 4
-  #     puts "=> #{@game.code_breaker}, you have guessed the secret code - #{@game.code}."
-  #     puts "=> Game over! Congratulations, you've won the game."
-  #   end
-  # end
-
-  def play_game
-    code = @game.create_code
-    puts "=> The #{@game.code_maker} has created the secret code."
-    puts "   [use]: 'GGGG' means all the green colors."
+  def end_game
     puts ''
-    feedback = 0
-    TURNS.times do |turn|
-      @game.turn = turn + 1
-      if @game.code_breaker.instance_of? Human
-        print "=> [#{turn + 1}]: #{@game.code_breaker}, please give your guess? "
-        guess = @game.guess_code
-      elsif @game.code_breaker.instance_of? Computer
-        guess = @game.guess_code
-        print "=> [#{turn + 1}]: #{@game.code_breaker}, please give your guess? #{guess}"
-        puts ''
-      end
-      feedback = @game.feedback(code, guess)
-      print_stats(guess, feedback)
-      break if feedback[0] == 4
+    if @game_status[0] == 4
+      puts "...Congratulations #{@game.code_breaker}, you have won the game."
+      puts "...You have successfully guessed the code: #{@game.code}."
+    else
+      puts "...#{@game.code_breaker}, you have exhausted all possible turns!"
+      puts '...Game over! You were unable to figure the code.'
     end
-    if feedback[0] != 4
-      puts "=> #{@game.code_breaker}, you have exhausted all the possible turns!"
-      puts '=> Game over! You were unable to find out the code.'
-    elsif feedback[0] == 4
-      puts "=> #{@game.code_breaker}, you have guessed the secret code - #{@game.code}."
-      puts "=> Game over! Congratulations, you've won the game."
+  end
+
+  def human_guess(turn)
+    puts ''
+    print "...#{turn + 1} Please enter your guess for the code: "
+    @game.guess_code
+  end
+
+  def computer_guess(turn)
+    puts ''
+    guess = @game.guess_code
+    print "...#{turn + 1} Computer has tried to make the guess #{guess}."
+    puts ''
+    guess
+  end
+
+  def play_game # rubocop:disable Metrics/MethodLength
+    code = @game.create_code
+    TURNS.times do |turn|
+      @game.turn = turn
+      if @game.code_breaker.instance_of? Human
+        guess = human_guess(turn)
+      elsif @game.code_breaker.instance_of? Computer
+        guess = computer_guess(turn)
+      end
+      @game_status = @game.feedback(code, guess)
+      print_result(guess, @game_status)
+      break if @game_status[0] == 4
     end
   end
 end
-
-mastermind = Mastermind.new
-mastermind.start_game
