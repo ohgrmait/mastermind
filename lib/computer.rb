6ae145851a7347822731dd1e79496c5799089df4
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2024-12-26 21:15:15 +0530

    Modify Computer class to guess code to refer game

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,15 +1,17 @@
-require 'debug'
+# frozen_string_literal: true
 
-class Computer
+class Computer # rubocop:disable Style/Documentation
   COLORS = %w[Blue Orange Yellow Green Purple Red].freeze
 
-  attr_accessor :patterns
+  attr_accessor :guess_patterns
 
   def initialize(game)
     # A total of 4 holes and 6 colors (6^4 = 1296 permutations)
-    @count = 0
     @game = game
     @patterns = COLORS.repeated_permutation(4).to_a
+    @guess_patterns = @patterns.map do |pattern|
+      pattern.map { |color| color[0] }.join
+    end
   end
 
   def create_code
@@ -18,19 +20,13 @@ class Computer
   end
 
   def guess_code
-    if @count.zero?
-      guess = 'BBOO'
-      @count += 1
-    else
-      # binding.break
-      # p patterns.size
-      patterns.select! do |pattern|
-        @game.feedback(@game.code, @game.guess) ==
-          @game.feedback(@game.guess, pattern.map { |elem| elem[0] }.join)
-      end
-      guess = patterns[0].map { |elem| elem[0] }.join
+    return 'BBOO' if @game.turn.zero?
+
+    guess_patterns.select! do |guess_pattern|
+      @game.feedback(@game.code, @game.guess) ==
+        @game.feedback(@game.guess, guess_pattern)
     end
-    guess
+    guess_patterns[0]
   end
 
   def to_s
